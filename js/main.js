67ffb5207920dc3cf1c5a7828c6cac1178ac8526
Yusuke Ogata <yusuke.ogata.yo@gmail.com>
2019-10-05 20:51:20 +0900

    complete!

---
+++ js/main.js
@@ -0,0 +1,28 @@
+  'use strict';
+{
+  console.log("message");
+
+  const num = 5;
+  const winner = Math.floor(Math.random() * num);
+
+  for( let i = 0; i < num; i++ ) {
+    const div = document.createElement('div');
+    div.classList.add('box');
+
+    div.addEventListener('click', () => {
+      if ( i === winner ) {
+  
+        div.textContent = 'Win!';
+        div.classList.add('win');
+      }
+      else {
+  
+        div.textContent = 'Lose!';
+        div.classList.add('lose');
+      }
+      div.classList.toggle('circle');
+    });
+
+    document.body.appendChild(div);
+  }
+}
